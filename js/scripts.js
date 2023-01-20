adebb53a82e541c3b8816b5ebf3e94b2d8b989f3
S-Ali-0 <118998558+S-Ali-0@users.noreply.github.com>
2023-01-20 21:28:52 +0000

    responsive mobile menu with functionality and added styles

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,27 @@ var typed = new Typed(".auto-type", {
     typeSpeed: 80,
     backSpeed: 30,
 })
+
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    var y = document.getElementById("topnavs");  
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+      y.style.backgroundPosition ="bottom left";
+    } else {
+      x.style.display = "flex";
+      x.style.flexDirection = "column";
+      y.style.backgroundPosition ="center";
+    }
+  }
+
+  
+
+function toggleIcon() {
+    let x = document.getElementById("menu-icon");  
+      x.classList.toggle('ri-close-circle-line');
+      x.classList.toggle('ri-menu-line');
+}  
+
+  
